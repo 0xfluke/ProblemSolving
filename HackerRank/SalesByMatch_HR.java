74218e090b43e8bd9f6fe6b37f65cfbd36be5a4b
0xfluke <160908987+0xfluke@users.noreply.github.com>
2026-01-10 22:58:42 +0300

    Update SalesByMatch_HR.java

--- HackerRank/SalesByMatch_HR.java
+++ HackerRank/SalesByMatch_HR.java
@@ -13,3 +13,20 @@ public static int sockMerchant(int n, List<Integer> ar) {
                 }
         return pairCounter/2; // Since the loop reads my placeholder pairs too, I need to divide the result by 2 to get rid of the ghost pairs
         }
+
+//This is the second approach based on the HashSet suggestion of Gemini
+public static int sockMerchant(int n, List<Integer> ar) {
+        int pairCounter = 0;
+        HashSet<Integer> sockSet = new HashSet<>();
+        for(int i = 0; i < ar.size(); i++){
+                if(!sockSet.contains(ar.get(i))){
+                        sockSet.add(ar.get(i));
+                }
+                else {
+                        sockSet.remove(ar.get(i));
+                        pairCounter++;
+                }
+        }
+        return pairCounter;
+
+    }
