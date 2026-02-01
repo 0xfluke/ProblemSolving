a9269393c70fc4ca434d586abfb81cc682ee4c42
0xfluke <160908987+0xfluke@users.noreply.github.com>
2026-02-01 22:05:58 +0300

    Update PlusOne_LC.java
    Correct Approach added

--- LeetCode/PlusOne_LC.java
+++ LeetCode/PlusOne_LC.java
@@ -14,4 +14,31 @@ public int[] plusOne(int[] digits) {
         return finalDigits;
     }
 
-// Second Approach
+// Second and Correct Approach
+public int[] plusOne(int[] digits) {
+        int carry = 1;
+        digits[digits.length-1] += carry;
+        int[] newDigits = new int[digits.length + 1];
+        for(int i = digits.length - 1; i >= 0 ;i--){
+            if(digits[i] <= 9){
+                return digits;
+            }
+            else if (digits[i] > 9) {
+                if(digits[0] > 9){
+                    newDigits[0] = 1;
+                    for(int j = 0; j< digits.length; j++){
+                        newDigits[j+1] = 0;
+                    }
+                    return newDigits;
+                }
+                digits[i] = 0;
+                digits[i-1]++;
+            }
+        }
+        if(newDigits[0] == 1){
+            return newDigits;
+        }
+        else{
+            return digits;
+        }
+    }
